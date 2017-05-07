6667001d35742be0b745881888b93efb757b2fe1
Julien <julien.pradet@gmail.com>
2017-05-07 13:38:05 +0200

    Add routine to manage form values

--- src/Chat/ChatForm.js
+++ src/Chat/ChatForm.js
@@ -1,17 +1,32 @@
 import dom from '../core/dom';
+import createEventHandler from '../core/createEventHandler';
 import Message from './Message.js';
 
 export default sendMessage => {
+  const { stream: message$, handler: updateMessage } = createEventHandler();
+
   const submitForm = event => {
     event.preventDefault();
     const formData = new FormData(event.target);
     sendMessage({ content: formData.get('message') });
+    updateMessage('');
   };
 
-  return dom.form(
-    { onsubmit: submitForm },
-    dom.label({ for: 'message' }, 'Message :'),
-    dom.input({ name: 'message', id: 'message' }),
-    dom.button({}, 'Envoyer')
+  const onChange = event => {
+    updateMessage(event.target.value);
+  };
+
+  return message$.startWith('').flatMap(messaveValue =>
+    dom.form(
+      { onsubmit: submitForm },
+      dom.label({ for: 'message' }, 'Message :'),
+      dom.input({
+        name: 'message',
+        id: 'message',
+        value: messaveValue,
+        onchange: onChange
+      }),
+      dom.button({}, 'Envoyer')
+    )
   );
 };
